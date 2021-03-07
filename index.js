f4ad2cf494331821de20f6dfa861b2f4093f1b89
moran-aga <77380388+moran-aga@users.noreply.github.com>
2021-03-07 15:01:27 +0200

    get a list of phonebook entries

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
+
+let persons = [
+ {
+  id: 1,
+  name: "Moran",
+  number: "0508947769",
+ },
+ {
+  id: 2,
+  name: "Dan",
+  number: "050123456",
+ },
+ {
+  id: 3,
+  name: "mom",
+  number: "050987654",
+ },
+];
 
 app.get("/", (request, response) => {
  response.send("<h1>Hello World!</h1>");
+});
+
+app.get("/api/persons", (request, response) => {
+ response.send(persons);
 });
 
 const PORT = 3002;
